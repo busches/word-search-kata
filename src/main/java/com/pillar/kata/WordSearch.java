091e2a403fdc042d31c7fa15306e2faba91c8760
Scott Busche <swbusche@gmail.com>
2019-05-21 21:34:05 -0500

    Remove logic from else
    Doesn't need to be in the else and removes a layer of identation too

--- src/main/java/com/pillar/kata/WordSearch.java
+++ src/main/java/com/pillar/kata/WordSearch.java
@@ -13,22 +13,22 @@ public class WordSearch {
     private void validateInput(File file) throws InvalidFileFormatException {
         if (!file.exists()) {
             throw new InvalidFileFormatException("Word Search File Not Found");
-        } else {
-            try {
-                long fileLineCount = Files.lines(file.toPath()).count();
-                if (fileLineCount < 3) {
-                    throw new InvalidFileFormatException("Word Search must contain at least three lines");
-                } else {
-                    List<String> entireFile = Files.readAllLines(file.toPath());
-                    if (entireFile.stream()
-                            .skip(1) // First row is lines to search
-                            .anyMatch(line -> line.split(",").length != fileLineCount)) {
-                        throw new InvalidFileFormatException("Word Search Grid must be a square");
-                    }
-                }
-            } catch (IOException e) {
-                throw new InvalidFileFormatException(e);
-            }
         }
+        try {
+            long fileLineCount = Files.lines(file.toPath()).count();
+            if (fileLineCount < 3) {
+                throw new InvalidFileFormatException("Word Search must contain at least three lines");
+            } else {
+                List<String> entireFile = Files.readAllLines(file.toPath());
+                if (entireFile.stream()
+                        .skip(1) // First row is words to search
+                        .anyMatch(line -> line.split(",").length != fileLineCount)) {
+                    throw new InvalidFileFormatException("Word Search Grid must be a square");
+                }
+            }
+        } catch (IOException e) {
+            throw new InvalidFileFormatException(e);
+        }
+
     }
 }

c52caaee1067616a42dd4d1766b9715a7e3a40c3
Scott Busche <swbusche@gmail.com>
2019-05-22 21:29:35 -0500

    Removed unneeded exception throws

--- src/main/java/com/pillar/kata/Solver.java
+++ src/main/java/com/pillar/kata/Solver.java
@@ -1,13 +1,12 @@
 package com.pillar.kata;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Solver {
-    public List<String> solve(WordSearch wordSearch) throws InvalidFileFormatException, IOException {
+    public List<String> solve(WordSearch wordSearch) {
 
         // This should likely return a List<String, List<Coordinate>> but keeping it simple for now
         List<String> foundWords = new ArrayList<>();

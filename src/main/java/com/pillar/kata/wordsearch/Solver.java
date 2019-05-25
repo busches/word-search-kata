aed9ea87c18161889061fed99dd9b5d56178f980
Scott Busche <swbusche@gmail.com>
2019-05-24 22:27:16 -0500

    Migrate final loop to stream

--- src/main/java/com/pillar/kata/wordsearch/Solver.java
+++ src/main/java/com/pillar/kata/wordsearch/Solver.java
@@ -7,8 +7,6 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-import static java.util.function.Predicate.not;
-
 public class Solver {
     public List<String> solve(WordSearch wordSearch) {
 
@@ -34,39 +32,33 @@ public class Solver {
     private Optional<String> searchGrid(List<String> grid, String wordToFind, SearchStrategy searchStrategy) {
         return IntStream.range(0, grid.size())
                 .filter(x -> x + wordToFind.length() * searchStrategy.xIncrement() <= grid.size())
-                .filter(x -> x + wordToFind.length() * searchStrategy.xIncrement() + 1 >= 0)
+                .filter(x -> x + wordToFind.length() * searchStrategy.xIncrement() >= -1)
                 .mapToObj(x -> loopX(grid, wordToFind, searchStrategy, x))
-                .filter(not(String::isBlank))
+                .filter(Optional::isPresent)
+                .map(Optional::get)
                 .findFirst();
     }
 
-    private String loopX(List<String> grid, String wordToFind, SearchStrategy searchStrategy, int x) {
+    private Optional<String> loopX(List<String> grid, String wordToFind, SearchStrategy searchStrategy, int x) {
         return IntStream.range(0, grid.size())
                 .filter(y -> y + wordToFind.length() * searchStrategy.yIncrement() <= grid.size())
-                .filter(y -> y + wordToFind.length() * searchStrategy.yIncrement() + 1 >= 0)
+                .filter(y -> y + wordToFind.length() * searchStrategy.yIncrement() >= -1)
                 .mapToObj(y -> loopY(grid, wordToFind, searchStrategy, x, y))
-                .collect(Collectors.joining());
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .findFirst();
     }
 
-    private String loopY(List<String> grid, String wordToFind, SearchStrategy searchStrategy, int x, int y) {
-        var foundWord = true;
-        var coordinates = "";
-        for (var letterPosition = 0; letterPosition < wordToFind.length(); letterPosition++) {
-            var currentX = x + letterPosition * searchStrategy.xIncrement();
-            var currentY = y + letterPosition * searchStrategy.yIncrement();
-            if (grid.get(currentY).split(",")[currentX].equals(wordToFind.substring(letterPosition, letterPosition + 1))) {
-                coordinates += String.format("(%s,%s)", currentX, currentY);
-                if (letterPosition != wordToFind.length() - 1) {
-                    coordinates += ",";
-                }
-            } else {
-                foundWord = false;
-                break;
-            }
-        }
-        if (foundWord) {
-            return String.format("%s: %s", wordToFind, coordinates);
-        }
-        return "";
+    private Optional<String> loopY(List<String> grid, String wordToFind, SearchStrategy searchStrategy, int x, int y) {
+        var wordFound = IntStream.range(0, wordToFind.length())
+                .allMatch(letterPosition ->
+                        grid.get(y + letterPosition * searchStrategy.yIncrement()).split(",")[x + letterPosition * searchStrategy.xIncrement()]
+                                .equals(wordToFind.substring(letterPosition, letterPosition + 1)));
+
+        return wordFound ? Optional.of(wordToFind + ": " + IntStream.range(0, wordToFind.length())
+                .mapToObj(letterPosition ->
+                        String.format("(%s,%s)", x + letterPosition * searchStrategy.xIncrement(), y + letterPosition * searchStrategy.yIncrement()))
+                .collect(Collectors.joining(",")))
+                : Optional.empty();
     }
 }
